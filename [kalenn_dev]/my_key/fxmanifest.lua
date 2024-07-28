88e2f37bd43814b2611059be0eec3f43103481d3
Kalenn <TSpydeur@gmail.com>
2024-07-28 10:00:43 +0200

    Update fxmanifest.lua

--- [kalenn_dev]/my_key/fxmanifest.lua
+++ [kalenn_dev]/my_key/fxmanifest.lua
@@ -1,18 +1,28 @@
 fx_version 'adamant'
-
 game 'gta5'
 
-description 'Ajoute un metier simplement'
-
+author 'VotreNom'
+description 'Système de verrouillage de véhicule avec clés pour esx_vehicleshop'
 version '1.0.0'
 
-client_script 'client.lua'
-server_script 'server.lua'
+shared_scripts {
+    '@es_extended/locale.lua',
+    'config.lua'
+}
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'client.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    '@es_extended/locale.lua',
+    'server.lua'
+}
 
 dependencies {
     'es_extended',
-    'mysql-async',
-    'esx_menu_default',
-    'esx_menu_list',
-    'esx_menu_dialog'
+    'esx_vehicleshop',
+    'mysql-async'
 }
